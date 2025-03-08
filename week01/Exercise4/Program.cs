62861e1e13a0b84d752d081c7a808e32081da132
NephiImo <imonephi@gmail.com>
2025-03-08 15:08:16 +0100

    Ex.4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -3,7 +3,28 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
-    }
+    {   
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)   
+            {
+                numbers.Add(number);
+            } 
+        }
+            int totalNumbers = numbers.Sum();
+            float averageNumbers = (float)numbers.Average();
+            int maxNumbers = numbers.Max();
+
+            Console.WriteLine($"The sum is: {totalNumbers}");
+            Console.WriteLine($"The average is: {averageNumbers}");
+            Console.WriteLine($"The largest number is: {maxNumbers}");
+        }   
+    
 }
