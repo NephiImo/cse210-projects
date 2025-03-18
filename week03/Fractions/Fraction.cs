4e53dace0bf929d32f13052fe9a844e45dd795b7
NephiImo <imonephi@gmail.com>
2025-03-18 01:54:08 +0100

    Fractions Completed!

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,32 @@
+class Fraction{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _numerator = top;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public string GetFractionString()
+    {
+        return$"{_numerator}/{_denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;
+    }
+}
