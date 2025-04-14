88aed3e37997093c8805cd8305bd0804bb610996
NephiImo <imonephi@gmail.com>
2025-04-15 00:04:29 +0100

    Eternal Quest Program Completed again

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -8,3 +8,8 @@ class Program
         manager.Start();
     }
 }
+
+// --------CREATIVITY SHOWN---------
+// I added a new class called AcievementManager that tracks the player's achievements.
+// This class has a method called CheckAchievements that checks if the player has reached certain milestones in their score or goals.
+// If they have, it prints a message to the console indicating that they have unlocked an achievement.
